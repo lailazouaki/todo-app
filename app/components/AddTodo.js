ce77c3dae9d5cc9e6b43fc1d23ba7e083e431a8b
Laila Zouaki <laila.zouaki@gmail.com>
2017-02-23 14:29:24 +1100

    Write tests for AddTodo component

--- app/components/AddTodo.js
+++ app/components/AddTodo.js
@@ -25,18 +25,18 @@ var AddTodo = React.createClass({
     render: function (){
         return (
             <div className='add-todo'>
-                <button onClick={this.openModal}>Add a new task</button>
+                <button id='add-task' onClick={this.openModal}>Add a new task</button>
                 <Modal
                     isOpen={this.state.modalIsOpen}
                     onRequestClose={this.closeModal}
                     contentLabel='Add a new task'>
 
                     <h2>Add a new task</h2>
-                    <button onClick={this.closeModal}>Close</button>
+                    <button id='close-modal' onClick={this.closeModal}>Close</button>
                     <div>What is the description of your task?</div>
                     <form>
-                        <input id="taskDescription"/>
-                        <button onClick={this.handleClickAddTask}>Save</button>
+                        <input id='taskDescription'/>
+                        <button id='save-task' onClick={this.handleClickAddTask}>Save</button>
                     </form>
                 </Modal>
             </div>
---
+++ test/AddTodo.spec.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+import { spy } from 'sinon';
+
+import AddTodo from '../app/components/AddTodo';
+
+describe('<AddTodo/>', function () {
+    it('should have all the addTask props', function (){
+        const wrapper = shallow(<AddTodo/>);
+        expect(wrapper.props().addTask).to.be.defined;
+    });
+
+    it('should start with a closed modal', function (){
+        const wrapper = shallow(<AddTodo/>);
+        expect(wrapper.state('modalIsOpen')).to.equal(false);
+    });
+
+    it('should render a modal', function (){
+        const wrapper = shallow(<AddTodo/>);
+        expect(wrapper.find('Modal')).to.have.length(1);
+    });
+
+    it('should have the right state depending on whether the modal is opened or not', function(){
+        const wrapper = shallow(<AddTodo/>);
+        const addButton = wrapper.find('#add-task');
+
+        expect(wrapper.state('modalIsOpen')).to.equal(false);
+        addButton.simulate('click');
+        expect(wrapper.state('modalIsOpen')).to.equal(true);
+
+        const closeModalButton = wrapper.find('#close-modal');
+        closeModalButton.simulate('click');
+        expect(wrapper.state('modalIsOpen')).to.equal(false);
+    });
+
+    it('should call the addTask props when `Save` is clicked', function(){
+        const wrapper = shallow(<AddTodo/>);
+        const saveButton = wrapper.find('#save-task')
+        expect(saveButton).to.have.length(1);
+
+        /* Could not manage to simulate the clicking of this button without
+        getting an error */
+    })
+})
